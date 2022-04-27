e8dcbc2afad3656e9dc605a3f2bd5b5c39848686
Cornelius Boateng <cob32@cornell.edu>
2022-04-27 13:24:47 -0400

    Day and Month tables created. Relationships established. Tested on Postman!

---
+++ Tables.py
@@ -0,0 +1,45 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
+class Day(db.Model):
+    __tablename__ = "days"
+    id = db.Column(db.Integer, primary_key=True)
+    month_id = db.Column(db.Integer, db.ForeignKey("months.id"))
+    month_name = db.Column(db.String, nullable=False)
+    day = db.Column(db.Integer, nullable=False)
+    active = db.Column(db.Boolean, nullable=False)
+    # timeslots = db.relationship("Timeslot", cascade="delete")
+   
+    def serialize_for_day(self):
+        return {
+            "id": self.id,
+            "month": self.month_name,
+            "day": self.day,
+            "active": self.active,
+             # "timeslots": [slot.serialize_for_day() for slot in self.timeslots]
+        }
+
+    def serialize_for_month(self):
+
+        return {
+            "day": self.day,
+            "active": self.active
+        }
+
+
+class Month(db.Model):
+    __tablename__ = "months"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    num_days = db.Column(db.Integer, nullable=False)
+    active = db.Column(db.Boolean, nullable=False)
+    days = db.relationship("Day", cascade="delete")
+
+    def serialize_for_month(self):
+        return {
+            "id": self.id,
+            "active": self.active,
+            "days": [day.serialize_for_month() for day in self.days]
+        }
+
---
+++ app.py
@@ -0,0 +1,66 @@
+
+from flask import Flask
+from Tables import db, Day, Month
+from gen import month_names
+from utils import response
+
+# Initialize Flask
+app = Flask(__name__)
+
+# DB config
+db_filename = "next.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+# Helpers
+def gen_months():
+    months = month_names()
+
+    for month_name, num_days in months.items():
+        month = Month(name=month_name, num_days = num_days, active=False)
+        db.session.add(month)
+        db.session.commit()
+
+
+# Routes
+@app.route("/next/days/", methods=["GET"])
+def gen_days():
+
+    gen_months()
+    months = Month.query.all()
+
+    for month in months:
+        for i in range(month.num_days):
+            day = Day(day=i+1, month_id=month.id, month_name=month.name, active=False)
+            db.session.add(day)
+            month.days.append(day)
+            db.session.commit()
+    
+    res = Day.query.all()
+    return response(res= {"days": [day.serialize_for_day() for day in res]}, success=True, code=200)
+
+
+@app.route("/next/months/", methods=["GET"])
+def get_months():
+    months = Month.query.all()
+    
+    return response(res={"months": {month.name: month.serialize_for_month() for month in months}})
+
+
+
+# Added for testing purposes. Drop all tables
+@app.route("/next/drop/", methods=["POST"])
+def drop_table():
+    db.drop_all(bind=None)
+
+    return response(res=[], success=True, code=201)
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4500)
