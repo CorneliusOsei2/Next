9a8889b2657a23b1df5228e8f81468c5def16d3a
Cornelius Boateng <cob32@cornell.edu>
2022-04-27 13:24:43 -0400

    Day and Month tables created. Relationships established. Tested on Postman!

---
+++ gen.py
@@ -0,0 +1,15 @@
+from Tables import Day, db
+
+def month_names():
+    return {
+     "January": 30,
+     "February": 28, # Check for leap  year
+     "March": 31,
+     "April": 30,
+     "May": 31,
+     "June": 30,
+     "July": 31,
+     "August": 31,
+     "September": 31
+    }
+
