896426f2f0575d5898256fb8445531cc9a0c76f1
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-04-30 15:34:07 -0400

    Current day made stateful. Timeslot buttons style changed.

--- frontend/next/src/Components/TimeslotButton/TimeslotButton.jsx
+++ frontend/next/src/Components/TimeslotButton/TimeslotButton.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useState } from 'react'
+import "./timeslotbuttons.css"
 
 
 const TimeslotButtons = ({state, handleJoinLeaveTimeslot}) => {
 
   const btn = {
     title:  state==="join"? "Join" : "Leave",
-    class: state==="join"? "join-queue" : "leave-queue"
+    class: state==="join"? "btn shadow-none join-queue btn-success" : "btn shadow-none leave-queue btn-danger"
   }
 
   const handleTimeslotButton = (e) => {
