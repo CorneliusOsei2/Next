267e83de6e5a25f44841cd9ee8db8ee6df42a67c
Cornelius Osei Owusu Boateng <78667952+CorneliusOsei2@users.noreply.github.com>
2022-05-01 16:45:57 -0400

    Update AddTimeslot.jsx

--- frontend/next/src/Components/AddTimeslot/AddTimeslot.jsx
+++ frontend/next/src/Components/AddTimeslot/AddTimeslot.jsx
@@ -1,16 +1,31 @@
-
-const AddTimeslot = () => {
+const AddTimeslot = ({course_id}) => {
 
     const addSlot = (e) => {
+        const slot = {
+            "start_time": e.target.start,
+            "end_time": e.target.end
+        }
 
+        fetch(`localhost:4500/{course_id}/add/`,
+            {'method':'POST',
+            headers : {
+            'Content-Type':'application/json'
+            },
+            body:JSON.stringify(body)
+            })
+        .then(response => response.json())
+        .catch(error => console.log(error))
     }
+    
 
     return (
         <div>
 
-            <form className="timeslot-form">
-                <input type="time" id="from" name="time" min="09:00" max="18:00" required></input>
-                <input type="time" id="to" name="time" min="09:00" max="18:00" required></input>
+            <form className="timeslot-form" onSubmit={addSlot}>
+                <input className="form-control" type="time" id="from" name="start" min="09:00" max="18:00" required></input>
+                <input className="form-control" type="time" id="to" name="end" min="09:00" max="18:00" required></input>
+
+                <button>Add</button>
             </form>
         </div>
     )
