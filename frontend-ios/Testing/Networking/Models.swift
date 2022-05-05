eb26468fe76cf6b05870f53ea997e9301f292894
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-05-05 18:42:31 -0400

    Networking models updated

--- frontend-ios/Testing/Networking/Models.swift
+++ frontend-ios/Testing/Networking/Models.swift
@@ -10,26 +10,36 @@ struct LoginResponse: Codable {
     let update_token: String
 }
 
-struct User{
-    let id: String
-    let name: String
-    let username: String
-    let courses_as_student: [Course]
-    let courses_as_instructor: [Course]
-//    let timeslots_as_students: [Timeslot]
-//    let timeslots_as_instructor: [Timeslot]
-}
-
 struct UserCoursesResponse: Codable {
     let user_id: String
     let courses_as_instructor: [Course]
     let courses_as_student: [Course]
 }
 
-struct Course: Codable {
-    let id: String
-    let code: String
+struct User{
+    let id: UUID?
     let name: String
+    let username: String
+    let courses_as_student: [Course]
+    let courses_as_instructor: [Course]
+    let timeslots_as_students: [Timeslot]
+    let timeslots_as_instructor: [Timeslot]
 }
 
+struct Course: Codable {
+    let id: UUID?
+    let code: String
+    let name: String
+    let instructors: [User]
+    let students: [User]
+}
+
+struct Timeslot: Codable {
+    let id: UUID?
+    let course_id: UUID?
+    let date: String
+    let start_time: String
+    let end_time: String
+    let total_joined: Int
+}
 
