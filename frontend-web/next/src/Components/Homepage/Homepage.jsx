9d32988c67889e42c5b20837e139d351eb344d61
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-05-14 12:58:41 -0400

    Homepage sidebar and topbar completed

--- frontend-web/next/src/Components/Homepage/Homepage.jsx
+++ frontend-web/next/src/Components/Homepage/Homepage.jsx
@@ -1,6 +1,6 @@
 import {init} from "ityped"
 import "./homepage.css"
-import profileIcon from "../../assets/images/profileIcon.png"
+import nextLogo from "../../assets/images/nextLogo.png"
 import { useEffect, useRef } from "react"
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
@@ -17,7 +17,6 @@ const Homepage = () => {
         <div className="homepage">
             <TopBar></TopBar>
             <Sidebar></Sidebar>
-            <TopPage></TopPage>
         </div>
     )
 }
@@ -30,22 +29,22 @@ const Sidebar = () => {
                 <ul>
                     <li className="sidebar-item">
                         <a href="index.html" title="user icons">
-                            <Tooltip title="Profile" placement="right"><AccountCircleIcon className="sidebar-icon" fontSize="large"></AccountCircleIcon></Tooltip>
+                            <Tooltip title="Profile" placement="right"><AccountCircleIcon color="success" className="sidebar-icon" fontSize="large"></AccountCircleIcon></Tooltip>
                         </a>
                     </li>
                     <li className="sidebar-item">
                         <a href="index.html" title="user icons">
-                            <Tooltip title="Courses" placement="right"><MenuBookIcon className="sidebar-icon" fontSize="large"></MenuBookIcon></Tooltip>
+                            <Tooltip title="Courses" placement="right"><MenuBookIcon color="success" className="sidebar-icon" fontSize="large"></MenuBookIcon></Tooltip>
                         </a>
                     </li>
                     <li className="sidebar-item">
                         <a href="index.html" title="user icons">
-                            <Tooltip title="Leave Feedback" placement="right"><FeedbackIcon className="sidebar-icon" fontSize="large"></FeedbackIcon></Tooltip>
+                            <Tooltip title="Leave Feedback" placement="right"><FeedbackIcon color="success" className="sidebar-icon" fontSize="large"></FeedbackIcon></Tooltip>
                         </a>
                     </li>
                     <li className="sidebar-item">
                         <a href="index.html" title="user icons">
-                            <Tooltip title="Logout" placement="right"><LogoutIcon className="sidebar-icon" fontSize="large"></LogoutIcon></Tooltip>
+                            <Tooltip title="Logout" placement="right"><LogoutIcon color="success" className="sidebar-icon" fontSize="large"></LogoutIcon></Tooltip>
                         </a>
                     </li>
 
@@ -57,47 +56,15 @@ const Sidebar = () => {
 
 const TopBar = () => {
     return (
-        <div className="notification">
-            <CircleNotificationsIcon className="notification-icon" style={{fontSize: 50}}></CircleNotificationsIcon>
-        </div>
-    )
-}
-
-const TopPage = () => {
-    const titleRef = useRef()
-    const descRef = useRef()
-
-    useEffect(() => {
-        init(titleRef.current, {
-            typeSpeed: 40,
-            showCursor: false,
-            strings: ['Corneli className="sidebar-item"us'],
-            backDelay: 50000,
-        })
-
-        init(descRef.current, {
-            typeSpeed: 40,
-            showCursor: true,
-            backDelay: 50000,
-            strings: ['A Computer Science Student at Cornell University interested in solving problems through efficient means! I like learning new stuff and writing code!']
-        })
-    }, [])
-
-    return (
-        <div className="toppage container">
-            <div className="row">
-                <div className="col-md-6">
-                    <div className="about">
-                    <h2 ref={titleRef} ></h2>
-
-                    <p ref={descRef}></p>
-                    </div>
-                </div>
-
-                <div className="col-md-6">
-                </div>
+        <div className="topbar">
+            <div className="next-logo-div">
+                <div><img className="next-logo" src={nextLogo} alt="" /></div>
+             </div>
+            <div className="notification">
+                <CircleNotificationsIcon className="notification-icon" style={{fontSize: 50}}></CircleNotificationsIcon>
             </div>
         </div>
+        
     )
 }
 
